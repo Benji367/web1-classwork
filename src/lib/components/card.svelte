d2501c1e097056e9290b921d6f3f0e2b90189e1a
benji367 <nikob2@student.op.ac.nz>
2026-05-15 10:36:39 +1200

    My first component

---
+++ src/lib/components/card.svelte
@@ -0,0 +1,11 @@
+<script>
+    let name = "Web 1"
+</script>
+
+<p>{name} Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus distinctio doloribus in voluptates animi t velit quo harum.</p>
+
+<style>
+    p{
+        color: goldenrod;
+    }
+</style>
