08b44fdbbc485e71d76291c0067a661eca935fd3
benji367 <nikob2@student.op.ac.nz>
2026-05-15 11:30:11 +1200

    Added images folder and image

--- src/lib/components/card.svelte
+++ src/lib/components/card.svelte
@@ -1,32 +1,49 @@
 <script>
-    let { name = "Web 1" } = $props();
+  let {
+    name = "Web 1",
+    image = "https://static.vecteezy.com/system/resources/thumbnails/002/534/006/small/social-media-chatting-online-blank-profile-picture-head-and-body-icon-people-standing-icon-grey-background-free-vector.jpg",
+  } = $props();
 </script>
 
-<p>{name} Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus distinctio doloribus in voluptates animi t velit quo harum.</p>
+<p>
+  {name} Lorem ipsum dolor sit amet consectetur adipisicing elit.
+</p>
 
 <div>
-    <img src="" alt="Placeholder" />
-    <aside>
-        <h3>{name}</h3>
-        <ul>
-            <li>Hobby 1</li>
-            <li>Hobby 2</li>
-            <li>Hobby 3</li>
-        </ul>
-    </aside>
-</div>
-<style>
-    div{
-        color: darkorange;
-        display: flex;
-        gap: 1em;
-        border: 1px solid grey;
-        margin-bottom: 1em;
-        padding: 1em;
-        max-width: 400px;
-    }
+  <img src={image} alt="Placeholder" />
+  
+  <video>
 
-    p{
-        color: goldenrod;
-    }
+  </video>
+
+  <aside>
+    <h3>{name}</h3>
+    <ul>
+      <li>Hobby 1</li>
+      <li>Hobby 2</li>
+      <li>Hobby 3</li>
+    </ul>
+  </aside>
+</div>
+
+<style>
+  div {
+    margin: 0 auto;
+    color: darkorange;
+    display: flex;
+    gap: 1em;
+    border: 1px solid grey;
+    margin-bottom: 1em;
+    padding: 1em;
+    max-width: 400px;
+  }
+
+  p {
+    color: goldenrod;
+  }
+
+  img{
+    margin: 0 auto;
+    max-width: 200px;
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,14 @@
 <script>
-    import Card from "$lib/components/card.svelte";    
+    import Card from "$lib/components/card.svelte";
+    import rubio from "$lib/images/rubio.jpg"    
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <img src="https://tse3.mm.bing.net/th/id/OIP.vrepPy4ZARZd4qsDrZ0dwAHaEK?rs=1&pid=ImgDetMain&o=7&rm=3" alt="F22 Jet">
 <Card name = {"Benjamin"}/>
-<Card name = {"Buddy"}/>
-<Card />
+<Card name = {"Buddy"} image={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc-NtG8CdrFyh9z9Kas0h4AEDGN5887RDzhxTXcV9bs4-vyPf2TZ6F0qNycorXrHO3xguANV_7zZhuVjLIFwoDoUacu1VW0OGuIaXN3X0S0N04UjzrFXSdwoirSKglfpk&s=10&ec=121691717"}/>
+<Card name = {"Marco"} image={rubio}/>
 <Card />
 
 <style>
@@ -28,6 +29,6 @@
     }
 
     img{
-        width: 180px;
+        width: 250px;
     }
 </style>
