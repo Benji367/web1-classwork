e385f6f6ead5cc66abbe10c6ae2d42ee652798cc
benji367 <nikob2@student.op.ac.nz>
2026-05-15 10:29:29 +1200

    Added javaScript, image, and styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+        let name = "Web 1"
+</script>
+
+<h1>Welcome to {name} SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<img src="https://tse3.mm.bing.net/th/id/OIP.vrepPy4ZARZd4qsDrZ0dwAHaEK?rs=1&pid=ImgDetMain&o=7&rm=3" alt="F22 Jet">
+
+<style>
+    p a{
+        color: red;
+        font: size 12px;
+        text-decoration: underline;
+    }
+
+    p{
+        color: lightseagreen;
+        font-size: 25px;
+
+    }
+
+    h1{
+        font-size: 40px;
+    }
+
+    img{
+        size: 100px, 100px;
+    }
+</style>
