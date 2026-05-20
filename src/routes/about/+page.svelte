610c46b1f1a90382eb2ff3bfdaf5257b87b44827
benji367 <nikob2@student.op.ac.nz>
2026-05-20 13:55:06 +1200

    Added about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<h1>About</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<img src="https://tse3.mm.bing.net/th/id/OIP.vrepPy4ZARZd4qsDrZ0dwAHaEK?rs=1&pid=ImgDetMain&o=7&rm=3" alt="F22 Jet">
+ 
+
+<style>
+    img {
+        width: 200px; 
+    }
+    
+</style>
